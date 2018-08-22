ce5728c69e18793f282cbeb67d3a224e8c1229f7
pawan12kamboj <41948730+pawan12kamboj@users.noreply.github.com>
2018-08-22 12:31:13 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,28 @@
+#question number-one
+print("hello world")
+
+#question number-two
+str1=("hello")
+str2=("world")
+print(str1+str2)
+
+#question number-three
+p=int(input("enter first number"))
+q=int(input("enter second number"))
+r=int(input("enter third number"))
+print("you have entered",p,q,r)
+
+#question number-four
+print("Let's get started ")
+
+#question number-five
+s="acadview"
+course="python"
+fees=5000
+print('\n s=%s \n course=%s \n fees=%d' %(s,course,fees))
+
+#question number-six
+PI = 3.14
+radius = float(input(' Please Enter the radius of a circle: '))
+area = PI * radius * radius
+print(" Area Of a Circle = %.2f" % area)
